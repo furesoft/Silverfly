88d5db1911880bfe1ab1ee1871d2d7ad4bf797da
Chris <filmee24@gmail.com>
2023-05-14 13:09:35 +0200

    refactor: Cleanup

--- Source/Test/TestParser.cs
+++ Source/Test/TestParser.cs
@@ -4,29 +4,21 @@ using Furesoft.PrattParser.Parselets;
 
 namespace Test;
 
-/// <summary>
-/// Extends the generic Parser class with support for parsing.
-/// </summary>
 public class TestParser : Parser<IExpression> {
-   public TestParser(Lexer lexer) : base(lexer) {
-
-      // Register all of the parselets for the grammar.
-
-      // Register the ones that need special parselets.
-      Register(PredefinedSymbols.Name, new NameParselet());
+   public TestParser(Lexer lexer) : base(lexer) 
+   { 
+       Register(PredefinedSymbols.Name, new NameParselet());
       
       Register("=", new AssignParselet());
       Register("?", new ConditionalParselet());
       Group("(", ")");
       Register(")", new CallParselet());
-
-      // Register the simple operator parselets.
+      
       Prefix("+", BindingPower.Prefix);
       Prefix("-", BindingPower.Prefix);
       Prefix("~", BindingPower.Prefix);
       Prefix("1", BindingPower.Prefix);
 
-      // For kicks, we'll make "!" both prefix and postfix, kind of like ++.
       Postfix("!", BindingPower.PostFix);
 
       InfixLeft("+", BindingPower.Sum);
