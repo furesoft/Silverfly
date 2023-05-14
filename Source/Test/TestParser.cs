1df0b2cc5b9e2a068b790390f580ef34b10818bc
Chris <filmee24@gmail.com>
2023-05-14 13:09:35 +0200

    refactor: Cleanup

--- Source/Test/TestParser.cs
+++ Source/Test/TestParser.cs
@@ -8,23 +8,24 @@ public class TestParser : Parser<IExpression> {
    public TestParser(Lexer lexer) : base(lexer) 
    { 
        Register(PredefinedSymbols.Name, new NameParselet());
-      
-      Register("=", new AssignParselet());
-      Register("?", new ConditionalParselet());
-      Group("(", ")");
-      Register(")", new CallParselet());
-      
-      Prefix("+", BindingPower.Prefix);
-      Prefix("-", BindingPower.Prefix);
-      Prefix("~", BindingPower.Prefix);
-      Prefix("1", BindingPower.Prefix);
-
-      Postfix("!", BindingPower.PostFix);
-
-      InfixLeft("+", BindingPower.Sum);
-      InfixLeft("-", BindingPower.Sum);
-      InfixLeft("*", BindingPower.Product);
-      InfixLeft("/", BindingPower.Product);
-      InfixRight("^", BindingPower.Exponent);
+       Register("=", new AssignParselet());
+       
+       Register("?", new ConditionalParselet()); 
+       Group("(", ")");
+       
+       Register(")", new CallParselet());
+       
+       Prefix("+", BindingPower.Prefix); 
+       Prefix("-", BindingPower.Prefix); 
+       Prefix("~", BindingPower.Prefix); 
+       Prefix("1", BindingPower.Prefix);
+       
+       Postfix("!", BindingPower.PostFix);
+       
+       InfixLeft("+", BindingPower.Sum); 
+       InfixLeft("-", BindingPower.Sum); 
+       InfixLeft("*", BindingPower.Product); 
+       InfixLeft("/", BindingPower.Product); 
+       InfixRight("^", BindingPower.Exponent);
    }
 }
