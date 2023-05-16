f093949da36196e56b45ea710989892d79e4441a
furesoft <filmee24@gmail.com>
2023-05-16 16:12:13 +0200

    feat(experimental): Add Automatic SourceRange Calculation For AstNodes

--- Source/Furesoft.PrattParser/Nodes/AstNode.cs
+++ Source/Furesoft.PrattParser/Nodes/AstNode.cs
@@ -1,9 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Furesoft.PrattParser.Nodes;
 
-public class AstNode
+public abstract class AstNode
 {
-    public SourceRange Range { get; set; }
-    
+    public virtual SourceRange Range => CalculateNodeRange();
+
+    // ToDo: Test Automatic SourceRange Calculation
+    private SourceRange CalculateNodeRange()
+    {
+        var sourceRanges = GetSourceRanges();
+        
+        SourceSpan minimumSpan = new(), maximumSpan = new();
+        
+        // find lowest source range
+        foreach (var sourceRange in sourceRanges)
+        {
+            if (sourceRange.Start < minimumSpan)
+            {
+                minimumSpan = sourceRange.Start;
+            }
+        }
+        
+        // find highest source range
+        foreach (var sourceRange in sourceRanges)
+        {
+            if (sourceRange.End > maximumSpan)
+            {
+                maximumSpan = sourceRange.End;
+            }
+        }
+
+        return new(minimumSpan, maximumSpan);
+    }
+
+    private IEnumerable<SourceRange> GetSourceRanges()
+    {
+        return GetType()
+            .GetProperties()
+            .Where(_ => _.PropertyType == typeof(SourceRange))
+            .Select(_=> (SourceRange)_.GetValue(this));
+    }
+
     public T Accept<T>(IVisitor<T> visitor)
     {
         return visitor.Visit(this);
