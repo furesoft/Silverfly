e3c7a7c1bc24de6997089207040e9c1612dc1c24
furesoft <filmee24@gmail.com>
2023-05-17 09:40:32 +0200

    feat: Add Method To Check If A Line/Colum Is In A SourceRange
    This is useful for writing tooling like LSP and autocompletion

--- Source/Furesoft.PrattParser/SourceRange.cs
+++ Source/Furesoft.PrattParser/SourceRange.cs
@@ -13,6 +13,21 @@ public struct SourceRange
     public SourceSpan Start { get; }
     public SourceSpan End { get; }
     public static SourceRange Empty { get; } = new();
+    
+    public bool Contains(int line, int column)
+    {
+        var inLeft = false;
+        if (Start.Line < line) inLeft = true;
+        else if (Start.Line > line) inLeft = false;
+        else inLeft = Start.Column <= column;
+
+        var inRight = false;
+        if (End.Line > line) inRight = true;
+        else if (End.Line < line) inRight = false;
+        else inRight = End.Column >= column;
+
+        return inLeft && inRight;
+    }
 
     public override string ToString()
     {
