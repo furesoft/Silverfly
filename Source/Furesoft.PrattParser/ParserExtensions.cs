1d3224cd5399ef9f480e691d4f4422b631b50998
Chris <filmee24@gmail.com>
2023-05-18 07:18:16 +0200

    feat: Add Helper Methods For Common Operators

---
+++ Source/Furesoft.PrattParser/ParserExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Furesoft.PrattParser.Parselets;
+using Furesoft.PrattParser.Parselets.Literals;
+
+namespace Furesoft.PrattParser;
+
+public static class ParserExtensions
+{
+    public static void AddArithmeticOperators<T>(this Parser<T> parser)
+    {
+        parser.Prefix("+", (int)BindingPower.Prefix);
+        parser.Prefix("-", (int)BindingPower.Prefix);
+        
+        parser.Group("(", ")");
+        
+        parser.InfixLeft("+", (int)BindingPower.Sum);
+        parser.InfixLeft("-", (int)BindingPower.Sum);
+        parser.InfixLeft("*", (int)BindingPower.Product);
+        parser.InfixLeft("/", (int)BindingPower.Product);
+    }
+    
+    public static void AddLogicalOperators<T>(this Parser<T> parser)
+    {
+        parser.Prefix("!", (int)BindingPower.Prefix);
+        
+        parser.InfixLeft("&&", (int)BindingPower.Product);
+        parser.InfixLeft("||", (int)BindingPower.Sum);
+    }
+    
+    public static void AddBitOperators<T>(this Parser<T> parser)
+    {
+        parser.Prefix("~", (int)BindingPower.Prefix);
+        
+        parser.InfixLeft("&", (int)BindingPower.Product);
+        parser.InfixLeft("|", (int)BindingPower.Sum);
+        
+        parser.InfixLeft("<<", (int)BindingPower.Product);
+        parser.InfixLeft(">>", (int)BindingPower.Product);
+    }
+
+    public static void AddCommonLiterals<T>(this Parser<T> parser)
+    {
+        parser.Register(PredefinedSymbols.Integer, (IInfixParselet<T>)new IntegerLiteralParselet());
+        parser.Register(PredefinedSymbols.Boolean, (IInfixParselet<T>)new BooleanLiteralParselet());
+    }
+}
--- Source/Test/TestParser.cs
+++ Source/Test/TestParser.cs
@@ -11,26 +11,22 @@ public class TestParser : Parser<AstNode>
     public TestParser(Lexer lexer) : base(lexer)
     {
         Register(PredefinedSymbols.Name, new NameParselet());
-        Register(PredefinedSymbols.Integer, new IntegerLiteralParselet());
+        
         Register("=", new AssignParselet());
 
         Register("(", new CallParselet());
-        Group("(", ")");
 
         Ternary("?", ":", (int)BindingPower.Conditional);
         
-        Prefix("+", (int)BindingPower.Prefix);
-        Prefix("-", (int)BindingPower.Prefix);
-        Prefix("~", (int)BindingPower.Prefix);
-        Prefix("!", (int)BindingPower.Prefix);
+        this.AddArithmeticOperators();
+        this.AddBitOperators();
+        this.AddLogicalOperators();
+        this.AddCommonLiterals();
+        
         Prefix("not", (int)BindingPower.Prefix);
 
         Postfix("!", (int)BindingPower.PostFix);
-
-        InfixLeft("+", (int)BindingPower.Sum);
-        InfixLeft("-", (int)BindingPower.Sum);
-        InfixLeft("*", (int)BindingPower.Product);
-        InfixLeft("/", (int)BindingPower.Product);
+        
         InfixRight("^", (int)BindingPower.Exponent);
         
         InfixLeft("->", (int)BindingPower.Product);
