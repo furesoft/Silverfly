850a87fc36ebda6c3ca14cfd243b53eeebe8f334
Chris <filmee24@gmail.com>
2024-06-04 05:46:44 +0000

    Add BlockNode To Visitor

--- Source/Furesoft.PrattParser/IVisitor.cs
+++ Source/Furesoft.PrattParser/IVisitor.cs
@@ -1,13 +1,31 @@
-﻿using Furesoft.PrattParser.Nodes;
+﻿using System.Collections.Generic;
+using Furesoft.PrattParser.Nodes;
 
 namespace Furesoft.PrattParser;
 
 public interface IVisitor<out TReturn>
 {
     TReturn Visit(AstNode node);
+    public IEnumerable<TReturn> Visit(BlockNode block) {
+        var result = new List<TReturn>();
+
+        foreach (var child in block.Children)
+        {
+            result.Add(Visit(child));
+        }
+
+        return result;
+    }
 }
 
 public interface IVisitor
 {
     void Visit(AstNode node);
+    public void Visit(BlockNode block)
+    {
+        foreach (var child in block.Children)
+        {
+            Visit(child);
+        }
+    }
 }
