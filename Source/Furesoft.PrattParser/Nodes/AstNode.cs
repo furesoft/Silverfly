65465e1a4cb74bf16467d0f6e6691eced8553e76
Chris <filmee24@gmail.com>
2024-06-25 14:15:51 +0000

    Pending changes exported from your codespace

--- Source/Furesoft.PrattParser/Nodes/AstNode.cs
+++ Source/Furesoft.PrattParser/Nodes/AstNode.cs
@@ -2,47 +2,18 @@ using Furesoft.PrattParser.Text;
 
 namespace Furesoft.PrattParser.Nodes;
 
-public abstract class AstNode
+//ToDo: convert AstNode to record
+public abstract record AstNode
 {
-    public SourceRange Range { get; private set; }
-    public AstNode Parent { get; private set; }
+    public SourceRange Range { get; init; }
+    public AstNode? Parent { get; init; }
 
-    public AstNode WithRange(Token token)
-    {
-        Range = new(token.Document, token.GetSourceSpanStart(), token.GetSourceSpanEnd());
-
-        return this;
-    }
-
-    public AstNode WithRange(SourceRange range)
-    {
-        Range = range;
-
-        return this;
-    }
-
-    public AstNode WithRange(SourceDocument document, SourceSpan start, SourceSpan end)
-    {
-        Range = new(document, start, end);
-
-        return this;
-    }
-
-    public AstNode WithRange(Token start, Token end)
-    {
-        Range = new(start.Document, start.GetSourceSpanStart(), end.GetSourceSpanEnd());
-
-        return this;
-    }
-
-    public AstNode WithParent(AstNode parent)
-    {
-        Parent = parent;
-
-        return this;
-    }
+    public AstNode WithRange(Token token) => this with { Range = new SourceRange(token.Document, token.GetSourceSpanStart(), token.GetSourceSpanEnd()) };
+    public AstNode WithRange(SourceRange range) => this with { Range = range };
+    public AstNode WithRange(SourceDocument document, SourceSpan start, SourceSpan end) => this with { Range = new SourceRange(document, start, end) };
+    public AstNode WithRange(Token start, Token end) => this with { Range = new SourceRange(start.Document, start.GetSourceSpanStart(), end.GetSourceSpanEnd()) };
+    public AstNode WithParent(AstNode parent) => this with { Parent = parent };
 
     public T Accept<T>(IVisitor<T> visitor) => visitor.Visit(this);
-
     public void Accept(IVisitor visitor) => visitor.Visit(this);
 }
