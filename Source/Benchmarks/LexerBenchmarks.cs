40837761713d997a915108358cf588ac88366685
Chris <filmee24@gmail.com>
2024-07-06 08:47:56 +0000

    chore: Add Core Fore Benchmarks

---
+++ Source/Benchmarks/LexerBenchmarks.cs
@@ -0,0 +1,20 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using Silverfly;
+
+namespace Benchmarks;
+
+[SimpleJob(RuntimeMoniker.Net80, baseline: true)]
+[MemoryDiagnoser]
+public class LexerBenchmarks
+{
+    [Benchmark]
+    public Token Number()
+    {
+        var lexer = new Lexer("42");
+
+        lexer.Next();
+
+        return lexer.Next();
+    }
+}
