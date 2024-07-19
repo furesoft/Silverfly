8d4b6d8dcbd3998cb12ab97b03277475ecca3e49
Chris <filmee24@gmail.com>
2024-07-19 15:13:18 +0200

    Update box.f

--- Source/Samples/Sample.FuncLanguage/box.f
+++ Source/Samples/Sample.FuncLanguage/box.f
@@ -1,5 +1,6 @@
 module Box;
 
+/* not yet implemented
 type Box(ref);
 
 let make val = new Box(val)
@@ -7,3 +8,4 @@ let set val box = box.ref = val
 let get box = box.ref
 
 let to_string b = b.ref
+*/
