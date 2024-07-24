4ce22038f0e2ac0bd0b6e9dffc0ac6aee0c44f75
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-07-24 11:58:54 +0200

    Cleanup

---
+++ Source/Samples/Sample.JSON/Parselets/ObjectParselet.cs
@@ -0,0 +1,13 @@
+using Silverfly;
+using Silverfly.Nodes;
+using Silverfly.Parselets;
+
+namespace Sample.JSON.Parselets;
+
+public class ObjectParselet : IStatementParselet
+{
+    public AstNode Parse(Parser parser, Token token)
+    {
+        return null;
+    }
+}
---
+++ Source/Samples/Sample.JSON/Program.cs
@@ -0,0 +1,19 @@
+﻿using Silverfly;
+
+namespace Sample.JSON;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            var input = Console.ReadLine();
+
+            var parsed = Parser.Parse<JsonGrammar>(input, useStatementsAtToplevel: true);
+            
+            Console.WriteLine(parsed.Tree.Accept(new PrintVisitor()));
+        }
+    }
+}
