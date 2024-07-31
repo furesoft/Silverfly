c4579bb634247f2691e86d745aa3175ce5baaec6
Chris Anders <filmee24@gmail.com>
2024-07-31 13:19:08 +0200

    feat: Add Naive Highlighting In Error Pretty Print

--- Source/Silverfly/Text/MessageFormatter.cs
+++ Source/Silverfly/Text/MessageFormatter.cs
@@ -17,7 +17,8 @@ internal static class MessageFormatter
             Console.Write($"  {lineNumber} ");
             Console.ResetColor();
 
-            Console.WriteLine($"│ {lineContent}");
+            Console.Write("│ ");
+            HighlightSyntax(lineContent);
 
             if (error.HighlightLines.Contains(error.Message.Range.Start.Line))
             {
@@ -27,9 +28,83 @@ internal static class MessageFormatter
                 Console.WriteLine($"     {underline}^");
             }
 
-            Console.WriteLine($"      {error.Message.Severity}: {error.Message.Text}");
+            Console.ForegroundColor = GetColorForSeverity(error.Message.Severity);
+            Console.Write($"      {error.Message.Severity}");
+            Console.ResetColor();
+            Console.WriteLine($": {error.Message.Text}");
         }
 
         Console.WriteLine("    ");
+    }
+
+    private static ConsoleColor GetColorForSeverity(MessageSeverity severity)
+    {
+        return severity switch
+        {
+            MessageSeverity.Error => ConsoleColor.Red,
+            MessageSeverity.Warning => ConsoleColor.Yellow,
+            MessageSeverity.Info => ConsoleColor.DarkGreen,
+            MessageSeverity.Hint => ConsoleColor.DarkYellow,
+        };
+    }
+
+    static void HighlightSyntax(string code)
+    {
+        string[] keywords = ["using", "class", "static", "void", "Console", "WriteLine"];
+        var keywordColor = ConsoleColor.Blue;
+        var stringColor = ConsoleColor.Green;
+
+        var lines = code.Split([Environment.NewLine], StringSplitOptions.None);
+
+        foreach (var line in lines)
+        {
+            var currentIndex = 0;
+            while (currentIndex < line.Length)
+            {
+                var isKeyword = false;
+
+                foreach (var keyword in keywords)
+                {
+                    if (!line[currentIndex..].StartsWith(keyword) ||
+                        (currentIndex + keyword.Length != line.Length &&
+                         char.IsLetterOrDigit(line[currentIndex + keyword.Length])))
+                    {
+                        continue;
+                    }
+
+                    Console.ForegroundColor = keywordColor;
+                    Console.Write(keyword);
+                    currentIndex += keyword.Length;
+                    isKeyword = true;
+                    break;
+                }
+
+                if (!isKeyword)
+                {
+                    if (line[currentIndex] == '"')
+                    {
+                        var closingQuoteIndex = line.IndexOf('"', currentIndex + 1);
+                        if (closingQuoteIndex == -1)
+                        {
+                            closingQuoteIndex = line.Length - 1;
+                        }
+
+                        Console.ForegroundColor = stringColor;
+                        Console.Write(line.Substring(currentIndex, closingQuoteIndex - currentIndex + 1));
+                        currentIndex = closingQuoteIndex + 1;
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                        Console.Write(line[currentIndex]);
+                        currentIndex++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ResetColor();
     }
 }
