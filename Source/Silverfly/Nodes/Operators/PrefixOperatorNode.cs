26143c05a0a271d979c9c0f908c60c2a1710c67f
Chris Anders <filmee24@gmail.com>
2024-08-10 08:20:19 +0200

    feat: Add Tag Properties To Prefix And Postifx

--- Source/Silverfly/Nodes/Operators/PrefixOperatorNode.cs
+++ Source/Silverfly/Nodes/Operators/PrefixOperatorNode.cs
@@ -3,6 +3,6 @@ namespace Silverfly.Nodes.Operators;
 /// <summary>
 /// A prefix unary arithmetic expression like "!a" or "-b".
 /// </summary>
-public record PrefixOperatorNode(Token Operator, AstNode Expr) : AstNode
+public record PrefixOperatorNode(Token Operator, AstNode Expr, string Tag) : AstNode
 {
 }
