2cb6c9ecedd2b67ed49b73f1d1a4310a47094a54
Chris Anders <filmee24@gmail.com>
2024-08-10 09:15:42 +0200

    feat: Add Initial Generation Stuff

---
+++ Source/Samples/Sample.FuncLanguage/Parselets/GeneratedParselet.cs
@@ -0,0 +1,10 @@
+﻿using Silverfly.Generator;
+using Silverfly.Parselets;
+
+namespace Silverfly.Sample.Func.Parselets;
+
+[Parselet("_def <id> '=' <expr>")]
+public partial class GeneratedParselet : IPrefixParselet
+{
+    
+}
