f85606788dd57b2cbc2735574165e5f28bba9b8e
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-16 15:41:30 +0200

    feat(rockstar): Add Booleans

---
+++ Source/Samples/Sample.Rockstar/Parselets/AliasedBooleanParselet.cs
@@ -0,0 +1,24 @@
+﻿using Sample.Rockstar.Matchers;
+using Silverfly;
+using Silverfly.Nodes;
+using Silverfly.Parselets;
+
+namespace Sample.Rockstar.Parselets;
+
+public class AliasedBooleanParselet : IPrefixParselet
+{
+    public AstNode Parse(Parser parser, Token token)
+    {
+        if (AliasedBooleanMatcher.TrueAliases.Contains(token.Text.ToString()))
+        {
+            return new LiteralNode(true, token).WithRange(token);
+        }
+        
+        if (AliasedBooleanMatcher.FalseAliases.Contains(token.Text.ToString()))
+        {
+            return new LiteralNode(false, token).WithRange(token);
+        }
+
+        return null;
+    }
+}
