7641f152f82c5a2d4dc4eeab4de4ccea4142e8f7
Chris Anders <filmee24@gmail.com>
2024-08-16 17:43:45 +0200

    refactor(rock): Cleanup

--- Source/Samples/Sample.Rockstar/RockstarGrammar.cs
+++ Source/Samples/Sample.Rockstar/RockstarGrammar.cs
@@ -12,19 +12,23 @@ public class RockstarGrammar : Parser
     protected override void InitLexer(LexerConfig lexer)
     {
         lexer.AddSymbols("(", ")");
-        
+        var emptyStringMatcher = new MappingMatcher("#empty_string", ["empty", "silent", "silence"]);
+        var nullStringMatcher = new MappingMatcher("#null", ["null", "nothing", "nowhere", "nobody", "gone"]);
+        var pronounMatcher = new MappingMatcher("#pronoun", ["it", "he", "she", "him", "her", "they", "them", "ze", "hir", "zie", "zir", "xe", "xem", "ve", "ver"]);
+
         lexer.AddKeywords(AliasedBooleanMatcher.TrueAliases);
         lexer.AddKeywords(AliasedBooleanMatcher.FalseAliases);
-        lexer.AddKeywords(EmptyStringMatcher.Aliases);
-        lexer.AddKeywords(PronounMatcher.Pronouns);
-        
+        lexer.AddKeywords(emptyStringMatcher.Aliases);
+        lexer.AddKeywords(pronounMatcher.Aliases);
+
         lexer.MatchNumber(false,false);
-        
+
         lexer.UseNameAdvancer(new RockstarNameAdvancer());
-        
+
         lexer.AddMatcher(new AliasedBooleanMatcher());
-        lexer.AddMatcher(new EmptyStringMatcher());
-        lexer.AddMatcher(new PronounMatcher());
+        lexer.AddMatcher(emptyStringMatcher);
+        lexer.AddMatcher(nullStringMatcher);
+        lexer.AddMatcher(pronounMatcher);
         
         lexer.IgnoreWhitespace();
         lexer.Ignore(new MultiLineCommentIgnoreMatcher("(", ")"));
@@ -36,7 +40,10 @@ public class RockstarGrammar : Parser
             separator: EOL);
         
         def.Register(PredefinedSymbols.Boolean, new AliasedBooleanParselet());
-        def.Register("#empty_string", new EmptyStringParselet());
+        def.Register("#empty_string", new MappingParselet(""));
+        def.Register("#null", new MappingParselet(null));
+        def.Register("#pronoun", new MappingParselet(null));
+
         def.Register(Name, new NameParselet());
     }
 }
