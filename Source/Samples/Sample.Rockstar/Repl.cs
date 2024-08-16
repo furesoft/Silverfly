3c168b38ccdaf9fd07ec0b01f27170dc0f83feb8
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-16 14:15:50 +0200

    feat(sample): Initial Setup For Rockstar

---
+++ Source/Samples/Sample.Rockstar/Repl.cs
@@ -0,0 +1,11 @@
+﻿using Silverfly.Repl;
+
+namespace Sample.Rockstar;
+
+public class Repl : ReplInstance<RockstarGrammar, RockstarCallbacks>
+{
+    protected override void Evaluate(string input)
+    {
+        var tu = Parser.Parse(input);
+    }
+}
---
+++ Source/Samples/Sample.Rockstar/RockstarGrammar.cs
@@ -0,0 +1,21 @@
+﻿using Silverfly;
+using Silverfly.Lexing.IgnoreMatcher.Comments;
+
+namespace Sample.Rockstar;
+
+public class RockstarGrammar : Parser
+{
+    protected override void InitLexer(LexerConfig lexer)
+    {
+        lexer.AddSymbols("(", ")");
+        
+        lexer.IgnoreWhitespace();
+        lexer.Ignore(new MultiLineCommentIgnoreMatcher("(", ")"));
+    }
+
+    protected override void InitParser(ParserDefinition def)
+    {
+        def.Block(PredefinedSymbols.SOF, PredefinedSymbols.EOF,
+            separator: PredefinedSymbols.EOL);
+    }
+}
