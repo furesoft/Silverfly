d4d2094a83d4358c44f405c89f3a3232030142f4
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-16 15:42:22 +0200

    feat(rock): Add EmptyString Parsing

---
+++ Source/Samples/Sample.Rockstar/Matchers/EmptyStringMatcher.cs
@@ -0,0 +1,29 @@
+﻿using Silverfly;
+using Silverfly.Lexing;
+
+namespace Sample.Rockstar.Matchers;
+
+public class EmptyStringMatcher : IMatcher
+{
+    public static readonly string[] Aliases = ["empty", "silent", "silence"];
+    public bool Match(Lexer lexer, char c)
+    {
+        return Aliases.Any(alias => lexer.IsMatch(alias));
+    }
+
+    public Token Build(Lexer lexer, ref int index, ref int column, ref int line)
+    {
+        var oldColumn = column;
+        var oldIndex = index;
+
+        foreach (var alias in Aliases)
+        {
+            if (lexer.IsMatch(alias))
+            {
+                lexer.Advance(alias.Length);
+            }
+        }
+
+        return new("#empty_string", lexer.Document.Source[oldIndex..index], line, oldColumn, lexer.Document);
+    }
+}
