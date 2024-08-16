e2a275071a696cb00176af785f9e7230414acdc9
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-16 15:41:30 +0200

    feat(rockstar): Add Booleans

---
+++ Source/Samples/Sample.Rockstar/Matchers/AliasedBooleanMatcher.cs
@@ -0,0 +1,32 @@
+﻿using Silverfly;
+using Silverfly.Lexing;
+
+namespace Sample.Rockstar.Matchers;
+
+public class AliasedBooleanMatcher : IMatcher
+{
+    public static readonly string[] TrueAliases = ["true", "right", "yes", "ok"];
+    public static readonly string[] FalseAliases = ["false", "wrong", "no", "lies"];
+    
+    public bool Match(Lexer lexer, char c)
+    {
+        return TrueAliases.Concat(FalseAliases).Any(alias => lexer.IsMatch(alias));
+    }
+
+    public Token Build(Lexer lexer, ref int index, ref int column, ref int line)
+    {
+        var oldColumn = column;
+        var oldIndex = index;
+
+        foreach (var alias in TrueAliases.Concat(FalseAliases))
+        {
+            if (lexer.IsMatch(alias))
+            {
+                lexer.Advance(alias.Length);
+                break;
+            }
+        }
+
+        return new(PredefinedSymbols.Boolean, lexer.Document.Source[oldIndex..index], line, oldColumn, lexer.Document);
+    }
+}
