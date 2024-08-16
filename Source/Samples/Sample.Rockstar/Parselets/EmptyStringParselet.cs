89da479adf7d917de693848f2a2c7ab09a8568f8
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-16 15:42:22 +0200

    feat(rock): Add EmptyString Parsing

---
+++ Source/Samples/Sample.Rockstar/Parselets/EmptyStringParselet.cs
@@ -0,0 +1,13 @@
+﻿using Silverfly;
+using Silverfly.Nodes;
+using Silverfly.Parselets;
+
+namespace Sample.Rockstar.Parselets;
+
+public class EmptyStringParselet : IPrefixParselet
+{
+    public AstNode Parse(Parser parser, Token token)
+    {
+        return new LiteralNode("", token).WithRange(token);
+    }
+}
