2fab3440889ac945985f86f5fde0feddb1e13e45
Chris Anders <chris.anders@schwaebisch-hall.de>
2024-08-19 13:43:10 +0200

    feat(sample-rock): Add Assignments

---
+++ Source/Samples/Sample.Rockstar/Parselets/AssignmentParselet.cs
@@ -0,0 +1,24 @@
+﻿using Silverfly;
+using Silverfly.Nodes;
+using Silverfly.Nodes.Operators;
+using Silverfly.Parselets;
+
+namespace Sample.Rockstar.Parselets;
+
+public class AssignmentParselet : IPrefixParselet
+{
+    // Put 123 into X
+    // Let my balance be 1000000
+    public AstNode Parse(Parser parser, Token token)
+    {
+        Symbol expectedToken = token.Type == "put" ? "into" : "be";
+
+        var name = parser.ParseExpression();
+
+        var operatorToken = parser.Consume(expectedToken);
+
+        var value = parser.ParseExpression();
+
+        return new BinaryOperatorNode(name, operatorToken.Rewrite("="), value).WithRange(token, parser.LookAhead(0));
+    }
+}
