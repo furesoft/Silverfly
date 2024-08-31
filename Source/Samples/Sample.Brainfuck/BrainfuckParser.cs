717182b3dea78865965ccc520cd54c68b398e89a
Chris Anders <filmee24@gmail.com>
2024-08-31 14:37:58 +0200

    feat(sample-brainf*ck): Add Brainfuck Parser

---
+++ Source/Samples/Sample.Brainfuck/BrainfuckParser.cs
@@ -0,0 +1,19 @@
+﻿using Sample.Brainfuck.Parselets;
+using Silverfly;
+
+namespace Sample.Brainfuck;
+
+public class BrainfuckParser : Parser
+{
+    protected override void InitLexer(LexerConfig lexer)
+    {
+
+    }
+
+    protected override void InitParser(ParserDefinition def)
+    {
+        def.Group("[", "]");
+
+        def.Register(new OperationParselet(), "+", "-", "<", ">", ".", ",");
+    }
+}
