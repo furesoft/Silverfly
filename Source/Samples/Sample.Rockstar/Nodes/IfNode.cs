135cabb0a26b2639954ac9cb798baa2222100bf2
Chris Anders <filmee24@gmail.com>
2024-09-08 18:37:06 +0200

    feat(sample-rock): Add if and while

---
+++ Source/Samples/Sample.Rockstar/Nodes/IfNode.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Immutable;
+using Silverfly.Nodes;
+
+namespace Silverfly.Sample.Rockstar.Nodes;
+
+public record IfNode(AstNode Condition, ImmutableList<AstNode> TruePart, ImmutableList<AstNode> FalsePart)
+    : StatementNode;
---
+++ Source/Samples/Sample.Rockstar/Parselets/LoopParselet.cs
@@ -0,0 +1,22 @@
+﻿using Silverfly.Nodes;
+using Silverfly.Parselets;
+using Silverfly.Sample.Rockstar.Nodes;
+
+namespace Silverfly.Sample.Rockstar.Parselets;
+
+public class LoopParselet : IStatementParselet
+{
+    public AstNode Parse(Parser parser, Token token)
+    {
+        var condition = parser.ParseExpression();
+
+        if (parser.IsMatch(",") || parser.IsMatch("\n"))
+        {
+            parser.Consume();
+        }
+
+        var body = parser.ParseList(PredefinedSymbols.EOF, "\n\n", ".");
+
+        return new LoopNode(condition, body);
+    }
+}
