73d3856cc448bee83a4262761c214300a0fbca75
Chris Anders <filmee24@gmail.com>
2024-10-03 16:51:42 +0200

    feat: Add EnumParselet

---
+++ Source/Silverfly/Parselets/EnumParselet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Silverfly.Nodes;
+
+namespace Silverfly.Parselets;
+
+public class EnumParselet<TRegister> : IPrefixParselet
+    where TRegister : struct
+{
+    public AstNode Parse(Parser parser, Token token)
+    {
+        var value = Enum.Parse<TRegister>(token.ToString(), true);
+
+        return new LiteralNode(value, token)
+            .WithRange(token);
+    }
+}
