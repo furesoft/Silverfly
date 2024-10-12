dfa50c0cf2849ad942eb0b6a9d1d8823a4e552fc
Chris Anders <filmee24@gmail.com>
2024-10-12 19:27:32 +0200

    feat: Add TypeName Parser

---
+++ Source/Silverfly/Helpers/GenericTypeName.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Immutable;
+
+namespace Silverfly.Helpers;
+
+public record GenericTypeName(Token Token, ImmutableList<TypeName> GenericArguments) : TypeName(Token)
+{
+
+}
