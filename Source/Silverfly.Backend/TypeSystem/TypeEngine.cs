7887fa511db1ed39950bd2666b981fe4d7b2be6f
codefactor-io <support@codefactor.io>
2024-11-06 16:45:28 +0000

    [CodeFactor] Apply fixes to commit b0580e1

--- Source/Silverfly.Backend/TypeSystem/TypeEngine.cs
+++ Source/Silverfly.Backend/TypeSystem/TypeEngine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -23,7 +23,6 @@ public class TypeEngine
             }
             catch (InvalidOperationException)
             {
-
             }
         }
 
