02b2fe0e6b2344ea61f9c8d53fc2b77bd3915dde
Chris <filmee24@gmail.com>
2025-08-25 09:00:46 +0200

    Update NameNode.cs

--- Source/Silverfly/Nodes/NameNode.cs
+++ Source/Silverfly/Nodes/NameNode.cs
@@ -8,10 +8,16 @@ public class NameNode : AstNode
     public NameNode(Token token)
     {
         Properties.Set(nameof(Token), token);
+        Properties.Set(nameof(Identifier), token.Text.ToString());
     }
 
     public Token Token
     {
         get => Properties.GetOrThrow<Token>(nameof(Token));
+    }
+
+    public string Identifier
+    {
+        get => Properties.GetOrThrow<string>(nameof(Identifier));
     }
 }
