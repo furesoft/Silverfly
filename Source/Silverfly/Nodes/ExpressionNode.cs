c04776fada8bccdcb36ab2801f63012f5267ec24
Chris <filmee24@gmail.com>
2025-08-25 09:38:56 +0200

    Create ExpressionNode.cs

---
+++ Source/Silverfly/Nodes/ExpressionNode.cs
@@ -0,0 +1,8 @@
+namespace Silverfly.Nodes;
+
+/// <summary>
+///     Represents an abstract statement node in an abstract syntax tree (AST).
+/// </summary>
+public abstract class ExpressionNode : AstNode
+{
+}
