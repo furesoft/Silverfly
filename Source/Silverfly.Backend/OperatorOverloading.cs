7fdafbff771d680064d62e3830445177a4a87f1e
Chris Anders <filmee24@gmail.com>
2025-09-28 10:55:07 +0200

    feat: Update BinMap and UnMap dictionaries to use tuples for operator metadata

--- Source/Silverfly.Backend/OperatorOverloading.cs
+++ Source/Silverfly.Backend/OperatorOverloading.cs
@@ -6,38 +6,38 @@ namespace Silverfly.Backend;
 
 public static class OperatorOverloading
 {
-    public static readonly Dictionary<string, string> BinMap = new()
+    public static readonly Dictionary<string, (string Name, bool isStatic)> BinMap = new()
     {
-        ["+"] = "op_Addition",
-        ["/"] = "op_Division",
-        ["-"] = "op_Subtraction",
-        ["*"] = "op_Multiply",
-        ["%"] = "op_Modulus",
+        ["+"] = ("op_Addition", true),
+        ["/"] = ("op_Division", true),
+        ["-"] = ("op_Subtraction", true),
+        ["*"] = ("op_Multiply", true),
+        ["%"] = ("op_Modulus", true),
 
-        ["&"] = "op_BitwiseAnd",
-        ["|"] = "op_BitwiseOr",
-        ["^"] = "op_ExclusiveOr",
-        ["=="] = "op_Equality",
-        ["!="] = "op_Inequality",
+        ["&"] = ("op_BitwiseAnd", true),
+        ["|"] = ("op_BitwiseOr", true),
+        ["^"] = ("op_ExclusiveOr", true),
+        ["=="] = ("op_Equality", true),
+        ["!="] = ("op_Inequality", true),
 
-        ["+="] = "op_AdditionAssignment",
-        ["-="] = "op_SubtractionAssignment",
-        ["*="] = "op_MultiplicationAssignment",
-        ["/="] = "op_DivisionAssignment",
+        ["+="] = ("op_AdditionAssignment", false),
+        ["-="] = ("op_SubtractionAssignment", false),
+        ["*="] = ("op_MultiplicationAssignment", false),
+        ["/="] = ("op_DivisionAssignment", false),
     };
 
-    public static readonly Dictionary<string, string> UnMap = new()
+    public static readonly Dictionary<string, (string Name, bool isStatic)> UnMap = new()
     {
-        ["!"] = "op_LogicalNot",
-        ["-"] = "op_UnaryNegation",
-        ["~"] = "op_OnesComplement",
+        ["!"] = ("op_LogicalNot", true),
+        ["-"] = ("op_UnaryNegation", true),
+        ["~"] = ("op_OnesComplement", true),
 
-        ["implicit"] = "op_Implicit",
-        ["explicit"] = "op_Explicit",
+        ["implicit"] = ("op_Implicit", true),
+        ["explicit"] = ("op_Explicit", true),
 
         // instance operators
-        ["++"] = "op_IncrementAssignment",
-        ["--"] = "op_DecrementAssignment",
+        ["++"] = ("op_IncrementAssignment", false),
+        ["--"] = ("op_DecrementAssignment", false),
     };
 
     public static bool TryGetOperator(this TypeDesc type, string op, out MethodDesc? opMethod, params TypeDesc[] args)
@@ -51,7 +51,7 @@ public static class OperatorOverloading
 
         if (nameMap!.TryGetValue(op, out var opMethodName))
         {
-            var candidate = type.FindMethod(opMethodName, new MethodSig(new TypeSig(), args.Select(t => new TypeSig(t)).ToList(), false), throwIfNotFound: false);
+            var candidate = type.FindMethod(opMethodName.Name, new MethodSig(new TypeSig(), args.Select(t => new TypeSig(t)).ToList(), false), throwIfNotFound: false);
 
             if (candidate != null)
             {
